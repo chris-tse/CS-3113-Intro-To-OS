2a2f75b3956b6ee68592ec995ff68af7949247ef
Christopher Tse <christopher-tse@outlook.com>
2017-10-09 19:11:04 -0500

    Add all needed commands

--- project1a/1a.c
+++ project1a/1a.c
@@ -1,43 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define BUFFER_SIZE 80
-#define MAX_ARGS 64
-#define SEPARATORS " \t\n"
+#define BUFFER_SIZE 1024                     // max line buffer
+#define MAX_ARGS 64                          // max num of args
+#define SEPARATORS " \t\n"                   // token separators
 
-int main(void)
+extern char **environ;                   // environment array
+
+int main(int argc, char **argv)
 {
     // initialize buffers and other arrays
-    char buf[BUFFER_SIZE];                // input buffer array
-    char *args[MAX_ARGS];                 // pointer to tokenized input array
-    char **arg;                           // pointer to loop through args
-    char *prompt = "1ash > ";             // prompt text
+    char buf[BUFFER_SIZE];                   // input buffer array
+    char *args[MAX_ARGS];                    // pointer to tokenized input array
+    char **arg;                              // pointer to loop through args
+    char *prompt = "amadeus > ";             // prompt text
 
+    system("clear");                         // clear terminal before taking in input
+
+    // continue reading input until quit command or redirected input source ends
     while(!feof(stdin))
     {
-        fputs(prompt, stdout);
 
-        if (fgets(buf, BUFFER_SIZE, stdin))
+        fputs(prompt, stdout);               // prompt user for commands
+
+        if (fgets(buf, BUFFER_SIZE, stdin))  // read in a line of command
         {
-            arg = args;
-            *arg++ = strtok(buf, SEPARATORS);
 
-            while( (*arg++ = strtok(NULL, SEPARATORS)) );
+            arg = args;
+            *arg++ = strtok(buf, SEPARATORS);                // tokenize and put into args array
+
+            while( (*arg++ = strtok(NULL, SEPARATORS)) );    // fill the remaining space in args with NULL
 
             if (args[0])
             {
                 if (!strcmp(args[0], "clr")) // clear command
                 {
-                    system("clear");
+                    system("clear");         // use system "clear" command
+                    continue;                // skip to next loop iteration
+                }
+
+                if (!strcmp(args[0], "dir")) // dir command
+                {
+                    char str[BUFFER_SIZE];
+                    strcpy(str, "ls -al ");
+                    if (args[1] == NULL)
+                    {
+                        system(str);
+                    }
+                    else
+                    {
+                        strcat(str, args[1]);
+                        system(str);
+                    }
+                    continue;
+                }
+
+                if (!strcmp(args[0], "environ"))
+                {
+                    // use pointer arithmetic to step through
+                    char **env = environ;
+                    while (*env)
+                        printf("%s\n", *env++);
+
                     continue;
                 }
 
                 if (!strcmp(args[0], "quit")) // quit command
-                    break;
+                    break;                    // break out of while input loop
 
                 // if neither, print command
                 arg = args;
-                while(*arg) fputs(*arg++, stdout);
+                while(*arg)
+                {
+                    fputs(*arg++, stdout);
+                    fputs(" ", stdout);
+                }
                 fputs("\n", stdout);
             }
         }
